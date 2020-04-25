7b0f9322e87e7bc4d477cd42c53f6a2614571351
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-25 20:53:21 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,12 +1,16 @@
 def find_min_value(array)
   count = 0 
+  max_value = -1
   min_value = 0 
   
   while count < array.length do 
-    if min_value > array[count]
+    if max_value < array[count]
       min_value = array[count]
     end
     count += 1 
+    if min_value < array[count]
+      min_value = array[count]
+    end
   end
   min_value
 end
